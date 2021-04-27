bf3e4825fb8ed8c52e110e403dafa06fcb757837
Ulysses-Starfall <419832946@qq.com>
2021-04-27 22:25:49 +0800

    feat(*):add quick sort run time compare

--- src/sort/quick_sort.py
+++ src/sort/quick_sort.py
@@ -37,10 +37,12 @@ def quick_sort2(data, start, end):
                 j -= 1
             if j > i:
                 data[i] = data[j]
+                i += 1
             while i < j and data[i] < pivot:
                 i += 1
             if i < j:
                 data[j] = data[i]
+                j -= 1
 
         data[i] = pivot
         quick_sort2(data, start, i - 1)
@@ -48,6 +50,20 @@ def quick_sort2(data, start, end):
 
 
 if __name__ == "__main__":
-    test_data = random.sample(range(1, 10), 5)
-    quick_sort2(test_data, 0, len(test_data) - 1)
-    print(test_data)
+    import time
+
+    start_time = int(time.time() * 1000)
+    for _ in range(100000):
+        test_data = random.sample(range(1, 10), 5)
+        quick_sort(test_data)
+    end_time = int(time.time() * 1000)
+
+    print("quick sort 1 total time: ", end_time - start_time)
+
+    start_time = int(time.time() * 1000)
+    for _ in range(100000):
+        test_data = random.sample(range(1, 10), 5)
+        quick_sort2(test_data, 0, len(test_data) - 1)
+    end_time = int(time.time() * 1000)
+
+    print("quick sort 2 total time: ", end_time - start_time)
